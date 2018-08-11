68cacc5023f589925b32fd469ee55a11ce5abb77
klivolks <klivolks@gmail.com>
2018-08-11 15:32:56 +0530

    v2.0.4

--- basic/settings/class_lib.php
+++ basic/settings/class_lib.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('Asia/Kolkata');
 include($_SERVER["DOCUMENT_ROOT"] .'/settings/db.php');
 include($_SERVER["DOCUMENT_ROOT"] .'/settings/input.php');
+include($_SERVER["DOCUMENT_ROOT"].'/functions.php');
 include($_SERVER["DOCUMENT_ROOT"] .'/settings/view.php');
 $load = new load;
 ?>
--- basic/settings/modal.php
+++ basic/settings/modal.php
@@ -1,5 +1,4 @@
 <?php
-include($_SERVER["DOCUMENT_ROOT"].'/functions.php');
 if($param[1]=='functions'){
 	$function = $param[2];
 	include($_SERVER["DOCUMENT_ROOT"].'/modal.php');
